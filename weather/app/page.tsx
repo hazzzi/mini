2db75d5da1d7d2dbf0eba0d5bac6599708e12e95
hazzzi <hanee_l@naver.com>
2024-05-16 23:26:18 +0900

    chore: Update weather app to fetch and display current weather

--- weather/app/page.tsx
+++ weather/app/page.tsx
@@ -1,22 +1,58 @@
 const weatherEmojis = {
-  thunderstorm: '⛈️',
-  drizzle: '🌧️',
-  rain: '🌧️',
-  snow: '❄️',
-  atmosphere: '🌫️',
-  clear: '☀️',
-  clouds: '☁️',
+  Thunderstorm: '⛈️',
+  Drizzle: '🌧️',
+  Rain: '🌧️',
+  Snow: '❄️',
+  Atmosphere: '🌫️',
+  Clear: '☀️',
+  Clouds: '☁️',
 };
 
-export default function Home() {
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+type Weather = {
+  name: string;
+  weather: Array<{
+    main:
+      | 'Thunderstorm'
+      | 'Drizzle'
+      | 'Rain'
+      | 'Snow'
+      | 'Atmosphere'
+      | 'Clear'
+      | 'Clouds';
+  }>;
+  main: {
+    temp: number;
+  };
+};
+
+export default async function Home() {
+  const latitude = 37.1289771;
+  const longitude = -84.0832646;
+
+  const searchParams = new URLSearchParams();
+  searchParams.set('lat', latitude.toString());
+  searchParams.set('lon', longitude.toString());
+  searchParams.set('appid', process.env.OPENWEATHERMAP_API_KEY ?? '');
+
+  const weather: Weather = await fetch(
+    `${baseUrl}?${searchParams.toString()}`,
+    {
+      cache: 'no-store',
+    }
+  ).then((res) => res.json());
+
+  const kelvinToCelsius = (kelvin: number) => Math.round(kelvin - 273.15);
+
   return (
     <main className="min-h-screen flex flex-col items-center justify-center gap-3">
-      <p className="text-5xl">{weatherEmojis['clear']}</p>
+      <p className="text-5xl">{weatherEmojis[weather.weather[0].main]}</p>
       <h1 className="flex font-bold">
-        <span className="text-8xl">19</span>
+        <span className="text-8xl">{kelvinToCelsius(weather.main.temp)}</span>
         <span className="text-xl">°C</span>
       </h1>
-      <p className="text-xl">리스본</p>
+      <p className="text-xl">{weather.name}</p>
     </main>
   );
 }
