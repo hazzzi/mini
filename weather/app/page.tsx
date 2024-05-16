3c86eecb848b990d358f39fbd972a4ac11b34b31
hazzzi <hanee_l@naver.com>
2024-05-16 23:15:55 +0900

    chore: Update background color and font in weather app

--- weather/app/page.tsx
+++ weather/app/page.tsx
@@ -1,14 +1,22 @@
+const weatherEmojis = {
+  thunderstorm: '⛈️',
+  drizzle: '🌧️',
+  rain: '🌧️',
+  snow: '❄️',
+  atmosphere: '🌫️',
+  clear: '☀️',
+  clouds: '☁️',
+};
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
-      <p>
-        날씨 이모지
-      </p>
-      <p>
-        <span>19</span>
-        <span>°C</span>
-      </p>
-      <p>Lisbon</p>
+    <main className="min-h-screen flex flex-col items-center justify-center gap-3">
+      <p className="text-5xl">{weatherEmojis['clear']}</p>
+      <h1 className="flex font-bold">
+        <span className="text-8xl">19</span>
+        <span className="text-xl">°C</span>
+      </h1>
+      <p className="text-xl">리스본</p>
     </main>
   );
 }
